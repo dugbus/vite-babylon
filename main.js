68ed51bf04b597966017b7e8f4e86b67e8e04e12
David Brown <2047439+dugbus@users.noreply.github.com>
2022-02-04 17:46:54 +0000

    added click detection

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import './styles.css'
 import * as BABYLON from 'babylonjs';
+import * as GUI from 'babylonjs-gui';
 
 var canvas = document.getElementById('renderCanvas');
 
@@ -8,6 +9,7 @@ var engine = new BABYLON.Engine(canvas, true, {preserveDrawingBuffer: true, sten
 var createScene = function() {
 
     var scene = new BABYLON.Scene(engine);
+    scene.clearColor = new BABYLON.Color4(0, 0, 0, 0);
 
     var camera = new BABYLON.ArcRotateCamera("Camera", -1, 1, 36, BABYLON.Vector3.Zero(), scene);
 
@@ -35,13 +37,46 @@ var createScene = function() {
       instance.position.y = 5 - Math.random() * 10;
       instance.position.z = 5 - Math.random() * 10;
 
+      instance.metadata = "box" + i;
+
       box.instancedBuffers.color = new BABYLON.Color4(1, Math.random(), Math.random(), 1);
 
+    }
+
+    scene.onPointerDown = function castRay(){
+        var ray = scene.createPickingRay(scene.pointerX, scene.pointerY, BABYLON.Matrix.Identity(), camera);
+
+        var hit = scene.pickWithRay(ray);
+
+        // if (hit.pickedMesh && hit.pickedMesh.metadata == "box"){
+        //   createGUIButton();
+        // }
+
+        if (hit.pickedMesh){
+            createGUIButton(hit);
+        }
     }
 
     scene.debugLayer.show({
       embedMode: true,
     });
+
+    function createGUIButton(hit){
+        let label = hit.pickedMesh.metadata;
+        //Creates a gui label to display the cannon
+        let guiCanvas = GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+        let guiButton = GUI.Button.CreateSimpleButton("guiButton", label);
+        guiButton.width = "150px"
+        guiButton.height = "40px";
+        guiButton.color = "white";
+        guiButton.cornerRadius = 5;
+        guiButton.background = "green";
+        guiButton.onPointerUpObservable.add(function() {
+            guiCanvas.dispose();
+        });
+        guiButton.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_CENTER;
+        guiCanvas.addControl(guiButton);
+    }
 
     return scene;
 }
