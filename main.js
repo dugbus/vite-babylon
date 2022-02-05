6a1ce407033bbebedd761eb59fda0a7b2bd65f5e
David Brown <2047439+dugbus@users.noreply.github.com>
2022-02-05 13:13:22 +0000

    added XR and moving cubes

--- main.js
+++ main.js
@@ -1,92 +1,114 @@
 import './styles.css'
-import * as BABYLON from 'babylonjs';
-import * as GUI from 'babylonjs-gui';
+import * as BABYLON from 'babylonjs'
+import * as GUI from 'babylonjs-gui'
 
-var canvas = document.getElementById('renderCanvas');
+var canvas = document.getElementById('renderCanvas')
 
-var engine = new BABYLON.Engine(canvas, true, {preserveDrawingBuffer: true, stencil: true});
+var engine = new BABYLON.Engine(canvas, true, {preserveDrawingBuffer: true, stencil: true})
+
+const getRandomInt = ( min, max ) => Math.floor( Math.random() * ( max - min ) ) + min
+
+var boxes = []
 
 var createScene = function() {
 
-    var scene = new BABYLON.Scene(engine);
-    scene.clearColor = new BABYLON.Color4(0, 0, 0, 0);
+  var scene = new BABYLON.Scene(engine)
+  scene.clearColor = new BABYLON.Color4(0, 0, 0, 0)
 
-    var camera = new BABYLON.ArcRotateCamera("Camera", -1, 1, 36, BABYLON.Vector3.Zero(), scene);
+  var camera = new BABYLON.ArcRotateCamera("Camera", -1, 1, 36, BABYLON.Vector3.Zero(), scene)
 
-    camera.attachControl(canvas, true);
+  camera.attachControl(canvas, true)
 
-    var count = 100;
+  var box = BABYLON.BoxBuilder.CreateBox("box", {size: 1}, scene)
 
-    var box = BABYLON.BoxBuilder.CreateBox("box", {size: 1}, scene);
+  var material = new BABYLON.StandardMaterial("white", scene)
 
-    var material = new BABYLON.StandardMaterial("white", scene);
+  material.diffuseColor = BABYLON.Color3.White()
 
-    material.diffuseColor = BABYLON.Color3.White();
+  box.material = material
 
-    box.material = material;
+  box.registerInstancedBuffer("color", 4)
+  box.instancedBuffers.color = new BABYLON.Color4(1, 0, 0, 1)
 
-    box.registerInstancedBuffer("color", 4);
-    box.instancedBuffers.color = new BABYLON.Color4(1, 0, 0, 1);
+  scene.createDefaultLight()
 
-    scene.createDefaultLight();
+  var count = 1000
 
-    for (var i = 0; i < count; i++) {
-      var instance = box.createInstance("box" + i);
+  for (var i = 0; i < count; i++) {
 
-      instance.position.x = 5 - Math.random() * 10;
-      instance.position.y = 5 - Math.random() * 10;
-      instance.position.z = 5 - Math.random() * 10;
+    var instance = box.createInstance("box" + i)
 
-      instance.metadata = "box" + i;
+    boxes.push(instance)
 
-      box.instancedBuffers.color = new BABYLON.Color4(1, Math.random(), Math.random(), 1);
+    instance.position.x = getRandomInt(-50, 50)
+    instance.position.y = getRandomInt(0, 150)
+    instance.position.z = getRandomInt(-50, 50)
 
+    instance.velocity = getRandomInt(1, 15)
+
+    instance.metadata = "box" + i
+
+    box.instancedBuffers.color = new BABYLON.Color4(1, Math.random(), Math.random(), 1)
+
+  }
+
+  console.log(boxes)
+
+  scene.onPointerDown = function castRay(){
+    var ray = scene.createPickingRay(scene.pointerX, scene.pointerY, BABYLON.Matrix.Identity(), camera)
+
+    var hit = scene.pickWithRay(ray)
+
+    // if (hit.pickedMesh && hit.pickedMesh.metadata == "box"){
+    //   createGUIButton();
+    // }
+
+    if (hit.pickedMesh){
+      createGUIButton(hit)
     }
 
-    scene.onPointerDown = function castRay(){
-        var ray = scene.createPickingRay(scene.pointerX, scene.pointerY, BABYLON.Matrix.Identity(), camera);
+  }
 
-        var hit = scene.pickWithRay(ray);
+  scene.debugLayer.show({
+    embedMode: true,
+  })
 
-        // if (hit.pickedMesh && hit.pickedMesh.metadata == "box"){
-        //   createGUIButton();
-        // }
+  function createGUIButton(hit) {
+    let label = hit.pickedMesh.metadata
+    //Creates a gui label to display the cannon
+    let guiCanvas = GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI")
+    let guiButton = GUI.Button.CreateSimpleButton("guiButton", label)
+    guiButton.width = "150px"
+    guiButton.height = "40px"
+    guiButton.color = "white"
+    guiButton.cornerRadius = 5
+    guiButton.background = "green"
 
-        if (hit.pickedMesh){
-            createGUIButton(hit);
-        }
-    }
+    guiButton.onPointerUpObservable.add(function() {
+      guiCanvas.dispose();
+    })
 
-    scene.debugLayer.show({
-      embedMode: true,
-    });
+    guiButton.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_CENTER
+    guiCanvas.addControl(guiButton)
+  }
 
-    function createGUIButton(hit){
-        let label = hit.pickedMesh.metadata;
-        //Creates a gui label to display the cannon
-        let guiCanvas = GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
-        let guiButton = GUI.Button.CreateSimpleButton("guiButton", label);
-        guiButton.width = "150px"
-        guiButton.height = "40px";
-        guiButton.color = "white";
-        guiButton.cornerRadius = 5;
-        guiButton.background = "green";
-        guiButton.onPointerUpObservable.add(function() {
-            guiCanvas.dispose();
-        });
-        guiButton.verticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_CENTER;
-        guiCanvas.addControl(guiButton);
-    }
+  const xr = scene.createDefaultXRExperienceAsync();
 
-    return scene;
+  return scene
 }
 
-var scene = createScene();
+var scene = createScene()
 
-engine.runRenderLoop(function(){
-    scene.render();
-});
+engine.runRenderLoop( function() {
+  for (var i = 0; i < boxes.length; i++) {
+    boxes[i].position.y = boxes[i].position.y - boxes[i].velocity / 100
+    if ( boxes[i].position.y < 0 ) {
+      boxes[i].position.y = 200
+    }
+  }
+  scene.render()
+} )
 
-window.addEventListener('resize', function(){
-    engine.resize();
-});
+window.addEventListener( 'resize', function() {
+  engine.resize()
+} )
